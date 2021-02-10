6fe55b7d9debb1acc704e51326e3479eb35fc4f2
francodicampli <francodicampli@gmail.com>
2021-02-10 18:39:13 -0300

    Marcas added

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\ProductController;
-use App\Http\Controllers\Api\UserController;
-use App\Http\Resources\ProductResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\ProductResource;
+use App\Http\Controllers\Api\DataController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
